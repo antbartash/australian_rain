ba5a380b420ce805905b6f92da6646f2d2d60748
Anton Bartash <ant.bartash@gmail.com>
2023-12-24 11:18:57 +0300

    Remove AUC since it's not implemented on GPU

--- MicrosoftNNI/model_GPU.py
+++ MicrosoftNNI/model_GPU.py
@@ -47,7 +47,7 @@ def get_default_parameters():
 def get_model(PARAMS):
     model = CatBoostClassifier(
         cat_features=['Location', 'WindGustDir', 'WindDir9am', 'WindDir3pm'],
-        custom_metric='AUC:hints=skip_train~false',
+        # custom_metric='AUC:hints=skip_train~false', # AUC is not implemented on GPU
         random_state=42, verbose=False, task_type='GPU'
     )
     model.set_params(**PARAMS)
@@ -64,7 +64,7 @@ def run(X_train, y_train, model):
     for rowid in iterations.index:
         metrics = {
             'default': iterations.loc[rowid, 'learn'][0],
-            'AUC': iterations.loc[rowid, 'learn'][1]
+            # 'AUC': iterations.loc[rowid, 'learn'][1] # AUC is not implemented on GPU
         }
         LOG.debug('mertics: ', metrics)
         nni.report_intermediate_result(metrics)
